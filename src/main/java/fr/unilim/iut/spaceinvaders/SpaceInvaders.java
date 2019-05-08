8c50f28ec09a36ec556abc37764498bb09dd23a7
lordgravier <baskette11@gmail.com>
2019-05-08 13:39:41 +0200

    positionner un nouveau vaisseau avec une dimension donnée

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -10,12 +10,13 @@ public class SpaceInvaders {
 	int longueur;
 	int hauteur;
 	Vaisseau vaisseau;
-    public SpaceInvaders(int longueur, int hauteur) {
-	   this.longueur = longueur;
-	   this.hauteur = hauteur;
-   }
-    
-    @Override
+
+	public SpaceInvaders(int longueur, int hauteur) {
+		this.longueur = longueur;
+		this.hauteur = hauteur;
+	}
+
+	@Override
 	public String toString() {
 		return recupererEspaceJeuDansChaineASCII();
 	}
@@ -24,7 +25,7 @@ public class SpaceInvaders {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
-				espaceDeJeu.append(recupererMarqueDeLaPosition(x,y));
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
 			espaceDeJeu.append(MARQUE_FIN_LIGNE);
 		}
@@ -34,9 +35,9 @@ public class SpaceInvaders {
 	public char recupererMarqueDeLaPosition(int x, int y) {
 		char marque;
 		if (this.aUnVaisseauQuiOccupeLaPosition(x, y)) {
-			marque=MARQUE_VAISSEAU;
+			marque = MARQUE_VAISSEAU;
 		} else {
-			marque=MARQUE_VIDE;
+			marque = MARQUE_VIDE;
 		}
 		return marque;
 	}
@@ -46,33 +47,31 @@ public class SpaceInvaders {
 	}
 
 	private boolean aUnVaisseau() {
-		return vaisseau!=null;
+		return vaisseau != null;
 	}
 
-    
-	public void positionnerUnNouveauVaisseau(int x, int y) {
-		
-		if (  !estDansEspaceJeu(x, y) )
+	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
+		if (!estDansEspaceJeu(x, y))
 			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
-	
-		vaisseau = new Vaisseau(x, y); 
 
+		vaisseau = new Vaisseau(longueur, hauteur);
+		vaisseau.positionner(x, y);
 	}
 
-	
-		
 	public boolean estDansEspaceJeu(int x, int y) {
 		return (((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur)));
 	}
 
 	public void deplacerVaisseauVersLaDroite() {
-		if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
-		
+		if (vaisseau.abscisseLaPlusADroite() < (longueur - 1))
+			vaisseau.seDeplacerVersLaDroite();
+
 	}
 
 	public void deplacerVaisseauVersLaGauche() {
-		if (vaisseau.abscisse()> 0) vaisseau.seDeplacerVersLaGauche();
-		
+		if (vaisseau.abscisseLaPlusAGauche() > 0)
+			vaisseau.seDeplacerVersLaGauche();
+
 	}
-    
+
 }
